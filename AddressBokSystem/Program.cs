496f8797c501651c6ab6172e9875069d48dde325
sonal <sonalkarle01@gmail.com>
2021-02-17 12:21:53 +0530

    Ability to create the contact

--- AddressBokSystem/Program.cs
+++ AddressBokSystem/Program.cs
@@ -3,58 +3,46 @@ using System.Collections.Generic;
 
 namespace AddressBookSystem
 {
-    class PersonInfo
+    class AddressBook
     {
-        //Variables
-        public string firstName;
-        public string lastName;
-        public string address;
-        public string city;
-        public string state;
-        public string zip;
-        public string phoneNumber;
-        public string email;
-    }
-    public class AddressBook
-    {
-        List<PersonInfo> addressBook;
-        public AddressBook()
+        //Create dictionary to store result
+        Dictionary<String, String> Contactlist = new Dictionary<string, string>();
+        private void AddContact()
         {
-            addressBook = new List<PersonInfo>();
+            Console.WriteLine("first Name:");
+            Contactlist.Add("first Name", Console.ReadLine());
+
+            Console.WriteLine("last Name:");
+            Contactlist.Add("last Name", Console.ReadLine());
+
+            Console.WriteLine("Address:");
+            Contactlist.Add("Address", Console.ReadLine());
+
+            Console.WriteLine("City:");
+            Contactlist.Add("City", Console.ReadLine());
+
+            Console.WriteLine("State:");
+            Contactlist.Add("State", Console.ReadLine());
+
+            Console.WriteLine("Zip:");
+            Contactlist.Add("Zip", Console.ReadLine());
+
+            Console.WriteLine("Phone number:");
+            Contactlist.Add("Phone number", Console.ReadLine());
+
+            Console.WriteLine("Email:");
+            Contactlist.Add("Email", Console.ReadLine());
         }
 
-
-    }
-    class Program
-    {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Address Book sysytem,");
             AddressBook addressBook = new AddressBook();
-            Console.WriteLine("Welcome to Address Book!");
-            int choice;
-            do
-            {
-                Console.WriteLine("Enter Choice:");
-                Console.WriteLine("1) Add Contact");
-                Console.WriteLine("2) Delete Contact");
-                Console.WriteLine("3) Exit");
-                choice = Convert.ToInt32(Console.ReadLine());
-                switch (choice)
-                {
-                    case 1:
-
-                        break;
-                    case 2:
-
-                        break;
-
-                    default:
-                        Console.WriteLine("Enter Proper Choice!");
-                        break;
-                }
-            } while (choice != 2);
-
-
+            addressBook.AddContact();
         }
+
+
     }
 }
+
+
